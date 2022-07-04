160384e921c5eaae27291cd17a9c03deaf9bfd7c
Rishita Kalluri <rishitakalluri45@gmail.com>
2022-07-04 10:44:41 +0530

    final commit

--- src/main/java/net/javaguides/springboot/h2/AuditController/auditController.java
+++ src/main/java/net/javaguides/springboot/h2/AuditController/auditController.java
@@ -1,12 +1,57 @@
 package net.javaguides.springboot.h2.AuditController;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import net.javaguides.springboot.h2.model.Details;
 
 @Controller
 public class auditController {
-    @RequestMapping("/")
-	public String home() {
+
+	public String answerOne,answerTwo,answerThree,projectName,managerName,applicationName;
+
+
+    @GetMapping("/")
+	public String home(Model model) {
+		Details details=new Details();
+		model.addAttribute("details", details);
+		return "Audit";
+	}
+
+	@PostMapping("/submit")
+	public String submit(@ModelAttribute("details") Details details){
+		System.out.println(details);
+		details.setProjectName(this.projectName);
+		details.setApplicationName(applicationName);
+		details.setManagerName(managerName);
+		int count=0;
+		if(details.getAnswerOne().equals("Yes"))
+			count++;
+		if(details.getAnswerTwo().equals("Yes"))
+			count++;
+		if(details.getAnswerThree().equals("Yes"))
+			count++;
+		if(details.getAnswerFour().equals("Yes"))
+			count++;
+		if(details.getAnswerFive().equals("Yes"))
+			count++;
+		details.setStatus(count>=3?"Green":"Red");
+		details.setAction(details.getStatus().equals("Green")?"No action required":"Action to be taken two weeks");
+		return "last";
+	}
+
+	@PostMapping("/submit/second")
+	public String submitSecond(@ModelAttribute("details") Details details){
+		System.out.println(details);
+		this.projectName=details.getProjectName();
+		this.applicationName=details.getApplicationName();
+		this.managerName=details.getManagerName();
 		return "Internal";
 	}
 }
--- src/main/java/net/javaguides/springboot/h2/model/Details.java
+++ src/main/java/net/javaguides/springboot/h2/model/Details.java
@@ -1,32 +1,72 @@
 package net.javaguides.springboot.h2.model;
 
+import java.util.Set;
+
+import javax.annotation.Generated;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Details {
- private Long id;
- private String projectName;
- private String projectManager;
- private String applicationOwner;
-public Long getId() {
-	return id;
-}
-public void setId(Long id) {
-	this.id = id;
-}
-public String getProjectName() {
-	return projectName;
-}
-public void setProjectName(String projectName) {
-	this.projectName = projectName;
-}
-public String getProjectManager() {
-	return projectManager;
-}
-public void setProjectManager(String projectManager) {
-	this.projectManager = projectManager;
-}
-public String getApplicationOwner() {
-	return applicationOwner;
-}
-public void setApplicationOwner(String applicationOwner) {
-	this.applicationOwner = applicationOwner;
-}
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	private String projectName;
+	private String managerName;
+	private String applicationName;
+	private String answerOne;
+	private String answerTwo;
+	private String answerThree;
+	private String status;
+	private String answerFour;
+	private String answerFive;
+	private String action;
+	public String getAnswerOne() {
+		return answerOne;
+	}public String getAnswerThree() {
+		return answerThree;
+	}public String getAnswerTwo() {
+		return answerTwo;
+	}public void setAnswerOne(String answerOne) {
+		this.answerOne = answerOne;
+	}public void setAnswerTwo(String answerTwo) {
+		this.answerTwo = answerTwo;
+	}public void setAnswerThree(String answerThree) {
+		this.answerThree = answerThree;
+	}public String getApplicationName() {
+		return applicationName;
+	}public String getManagerName() {
+		return managerName;
+	}public String getProjectName() {
+		return projectName;
+	}public void setApplicationName(String applicationName) {
+		this.applicationName = applicationName;
+	}public void setManagerName(String managerName) {
+		this.managerName = managerName;
+	}public void setProjectName(String projectName) {
+		this.projectName = projectName;
+	}public String getStatus() {
+		return status;
+	}public void setStatus(String status) {
+		this.status = status;
+	}public String getAnswerFour() {
+		return answerFour;
+	}public String getAnswerFive() {
+		return answerFive;
+	}public void setAnswerFive(String answerFive) {
+		this.answerFive = answerFive;
+	}public void setAnswerFour(String answerFour) {
+		this.answerFour = answerFour;
+	}public String getAction() {
+		return action;
+	}public void setAction(String action) {
+		this.action = action;
+	}
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return answerOne+" "+answerTwo+" "+answerThree+" "+projectName+" "+managerName+" "+applicationName;
+	}
 }
